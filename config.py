c2abe15d72357fdc8384a1e7692636b2f4bb2d98
Edigaryev Matvey <matthewsantoro@protonmail.com>
2020-08-31 21:09:09 +0500

    fix req

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 TELEGRAM_API_ID = os.environ.get('TELEGRAM_API_ID')
-API_HASH = os.environ.get('TELEGRAM_API_ID')
+API_HASH = os.environ.get('API_HASH')
 
 WEATHER_API = os.environ.get('WEATHER_API')
 CITY_ID = os.environ.get('CITY_ID')
