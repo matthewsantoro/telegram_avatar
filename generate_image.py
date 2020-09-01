24fbcaad25774f7da8cc863baace9ce33a49cdb3
matthew <matthewsantoro@protonmail.com>
2020-09-02 01:12:09 +0500

    delete useless file, rewrite text area

--- generate_image.py
+++ generate_image.py
@@ -18,21 +18,37 @@ def generate_image(data):
         int((background_w - icon_w) / 2),
         int((background_h - icon_h) / 2),
         int((background_w - icon_w) / 2 + icon_w),
-        int((background_h - icon_h) / 2 + icon_w)
+        int((background_h - icon_h) / 2 + icon_h)
     )
     background.paste(icon, area, icon)
     font = ImageFont.truetype("font/YanoneKaffeesatz-VariableFont_wght.ttf", 100)
 
     img_text = Image.new("RGBA", background.size, (255, 255, 255, 0))
     d = ImageDraw.Draw(img_text)
+
     text = data['dt']
     text_w, text_h = font.getsize(text)
-    d.text((int(background_w / 2 - text_w / 2),
-            int(background_h / 8 - text_h / 2)), text, font=font, fill=(255, 255, 255, 255))
+    print(text_h)
+    text_area = (int((background_w - text_w) / 2),
+                 int(area[1] / 2 - text_h / 2))
+
+    d.text(text_area, text, font=font, fill=(255, 255, 255, 255))
 
     text = f'{data["temp"]}°C'
     text_w, text_h = font.getsize(text)
-    d.text((int(background_w / 2 - text_w / 2),
-            int(7 * background_h / 8 - text_h / 2)), text, font=font, fill=(255, 255, 255, 255))
+    print(text_h)
+    text_area = (int((background_w - text_w) / 2),
+                 int(background_h - (area[1] / 2 + text_h / 2)))
+    d.text(text_area, text, font=font, fill=(255, 255, 255, 255))
+
     result = Image.alpha_composite(background, img_text)
+    result.show()
     result.save('images/result.png', 'PNG')
+
+
+data = {'dt': '22:00',
+        'temp': '17',
+        'icon': '04d'}
+
+generate_image(data)
+
